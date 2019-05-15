11865b78695621b557d565b077549008df176cf9
Aldarien <aldarien85@gmail.com>
2019-05-15 13:50:00 -0400

    FIX: type hint

--- menu_models/app/Middleware/ModelMigrator.php
+++ menu_models/app/Middleware/ModelMigrator.php
@@ -2,6 +2,8 @@
 namespace App\Middleware;
 
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use App\Service\Migrator;
 
 class ModelMigrator {
@@ -10,7 +12,7 @@ class ModelMigrator {
   public function __construct(ContainerInterface $container){
     $this->container = $container;
   }
-  public function __invoke($request, $response, $next) {
+  public function __invoke(RequestInterface $request, ResponseInterface $response, $next) {
     $migrator = new Migrator($this->container);
     $migrator->load($this->container->cfg->get('locations.models.migrations'))->check()->migrate();
     return $next($request, $response);
